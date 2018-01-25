578e6353155d9f791553d534532ba26017e9de3c
bishoyabd <bishoyabd@gmail.com>
2018-01-25 03:27:21 +0200

    fixed bug in seekBar

--- app/src/main/java/com/project/bishoy/soleektask/addplan/AddPlanActivity.java
+++ app/src/main/java/com/project/bishoy/soleektask/addplan/AddPlanActivity.java
@@ -3,8 +3,13 @@ package com.project.bishoy.soleektask.addplan;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.project.bishoy.soleektask.R;
+import com.warkiz.widget.IndicatorSeekBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by bisho on 1/24/2018.
@@ -12,9 +17,49 @@ import com.project.bishoy.soleektask.R;
 
 public class AddPlanActivity extends AppCompatActivity {
 
+    @BindView(R.id.seekBar_attendees)
+    IndicatorSeekBar seekBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_plan_activity);
+        ButterKnife.bind(this);
+        setupSeekBar();
+
     }
+
+    //
+    private void setupSeekBar() {
+
+        seekBar.setOnSeekChangeListener(new IndicatorSeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(IndicatorSeekBar seekBar, int progress, float progressFloat, boolean fromUserTouch) {
+
+                this.progress = progress / 50;
+                this.progress = progress * 50;
+                String s = String.valueOf(progress);
+                Log.d("plan", "progress--> " + this.progress);
+            }
+
+            @Override
+            public void onSectionChanged(IndicatorSeekBar seekBar, int thumbPosOnTick, String textBelowTick, boolean fromUserTouch) {
+                //only callback on discrete series SeekBar type.
+            }
+
+            @Override
+            public void onStartTrackingTouch(IndicatorSeekBar seekBar, int thumbPosOnTick) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(IndicatorSeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+
 }
